9890461cef7a23714bc07041869da927bd431efb
Nicolas <pinier74nicolas@gmail.com>
2022-08-25 16:46:42 +0200

    change a problematic migration file Version20220819125219

--- migrations/Version20220819125219.php
+++ migrations/Version20220819125219.php
@@ -22,13 +22,11 @@ final class Version20220819125219 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE curriculum (id INT AUTO_INCREMENT NOT NULL, candidate_id_id INT DEFAULT NULL, file_name VARCHAR(60) NOT NULL, file VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7BE2A7C347A475AB (candidate_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE curriculum ADD CONSTRAINT FK_7BE2A7C347A475AB FOREIGN KEY (candidate_id_id) REFERENCES candidate (id)');
-        $this->addSql('ALTER TABLE candidate DROP cv');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE curriculum');
-        $this->addSql('ALTER TABLE candidate ADD cv VARCHAR(255) DEFAULT NULL');
     }
 }
